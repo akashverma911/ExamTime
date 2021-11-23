25bc468c42795bd316348c2b816d455375c9859b
vikasverma911 <69479320+vikasverma911@users.noreply.github.com>
2021-11-23 22:38:52 +0530

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+# Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can then be easily rendered into JSON, XML or other content types. 
 from rest_framework import serializers
 from rest_auth.registration.serializers import RegisterSerializer
 from .models import User
