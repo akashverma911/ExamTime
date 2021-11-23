deaf3f03a10dafb744ef2c5f6799a5df2fdea67f
vikasverma911 <69479320+vikasverma911@users.noreply.github.com>
2021-11-23 22:20:46 +0530

    Update actionTypes.js

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,3 +1,4 @@
+// This file contains major actions which on dispatch changes the state of object
 export const AUTH_START = "AUTH_START";
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAIL = "AUTH_FAIL";
