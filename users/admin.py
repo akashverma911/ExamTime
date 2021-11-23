3a86eac730d8f6324d6bd091d285dee62a8e9511
vikasverma911 <69479320+vikasverma911@users.noreply.github.com>
2021-11-23 22:35:37 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
+# File contains the file relating to ui of admin page
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
